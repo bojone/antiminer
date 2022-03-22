94730606299c7c3cc34d3ad1e3f142dcbb197a69
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2022-03-22 14:18:58 +0800

    Create anti_miner.py

---
+++ anti_miner.py
@@ -0,0 +1,43 @@
+#! -*- coding: utf-8 -8-
+# 监控查杀挖矿病毒
+
+import os
+import psutil
+import datetime
+import json
+import time
+import sys
+
+fw = open('kill.log', 'a+')
+
+
+def check(p):
+    path, _ = os.path.split(p.exe())
+    if os.path.exists(path + '/lolMiner.cfg'):
+        return True
+
+
+def do(p):
+    log = {'time': str(datetime.datetime.now()), 'path': p.exe()}
+    print(log)
+    fw.write(json.dumps(log) + '\n')
+    fw.flush()
+    p.kill()
+    os.system('sudo rm %s' % log['path'])
+    os.system('sudo touch %s' % log['path'])
+    os.system('sudo chattr +i %s' % log['path'])
+
+
+def wait():
+    for i in range(1, 6):
+        sys.stdout.write(u'\r监控中' + '.' * i)
+        sys.stdout.flush()
+        time.sleep(1)
+    sys.stdout.write('\r' + ' ' * 100 + '\r')
+
+
+while True:
+    for p in psutil.process_iter():
+        if check(p):
+            do(p)
+    wait()
